7502e314752adbdfa665ae3d07fdd619b1e1bb34
Rob Laine <rlainewyllie@gmail.com>
2019-11-25 19:05:45 +1100

    Initial commit

---
+++ snake.py
@@ -0,0 +1,24 @@
+import time
+import pygame
+
+
+class Player:
+    def __init__():
+        self.x = 0
+        self.y = 0
+        self.speed = [1, 1]
+
+class App:
+    def __init__():
+        pygame.init()
+
+if __name__ == '__main__':
+    a = App
+    size = width, height = 320, 240
+    speed = [1,1]
+
+    while 1:
+
+    
+
+
