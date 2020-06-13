8c22dd8aa4b9539614c4bf3ff2c3c6c885be704d
Rob Laine <rlainewyllie@gmail.com>
2020-06-13 14:29:29 +1000

    Implement ball movement in x and y axes

--- pong/ball/ball.py
+++ pong/ball/ball.py
@@ -6,7 +6,11 @@ from os import getcwd
 from os.path import join, abspath
 import pygame
 from utils.constants import (
-    INITIAL_BALL_RAD, INITIAL_BALL_SPEED, INITIAL_BALL_POS, BOARD_WIDTH
+    INITIAL_BALL_RAD,
+    INITIAL_BALL_SPEED,
+    INITIAL_BALL_POS,
+    BOARD_WIDTH,
+    BOARD_HEIGHT
 )
 
 
@@ -21,29 +25,44 @@ class Ball:
         """
         self.coords = INITIAL_BALL_POS
         self.diameter = 2 * INITIAL_BALL_RAD
-        self.speed = INITIAL_BALL_SPEED
+        self.speed = (5, 5)  # INITIAL_BALL_SPEED
+        # V: 1 for down, -1 for up; H: 1 for right, -1 for left
+        self.direction = {'V': 1, 'H': 1}
         self.color = (255, 0, 255)
         self.image = pygame.transform.scale(pygame.image.load(
-            join(abspath(getcwd()), 'pong', 'images', 'intro_ball.gif')).convert(), (self.diameter, self.diameter))
+            join(abspath(getcwd()), 'pong', 'images', 'intro_ball.gif')).convert(),
+            (self.diameter, self.diameter))
 
     def update(self):
-        """Updates the position of the ball.
+        """Updates the position of the ball, and handles the logic for when
+        the ball collides with the edges of the board
         """
         old_coords = self.coords
-        # Moving in the left-right direction its within the bounds of the board
-        if old_coords[0] + self.diameter <= BOARD_WIDTH and old_coords[0] >= 0:
-            new_coords = (old_coords[0] + self.speed[0], old_coords[1])
-        else:
-            new_coords = self.coords
+        # Handle the horizontal movement of the ball
+        if old_coords[0] + self.diameter >= BOARD_WIDTH or old_coords[0] <= 0:
+            # Reverse the direction when we exceed or meet the bounds of the
+            # board either left or right
+            self.direction['H'] = -(self.direction['H'])
+
+        # Handle the vertical movement of the ball
+        if old_coords[1] + self.diameter >= BOARD_HEIGHT or old_coords[1] <= 0:
+            # Reverse the direction when we exceed or meet the bounds of the
+            # board either left or right
+            self.direction['V'] = -(self.direction['V'])
+
+        new_coords = (old_coords[0] + self.direction['H'] *
+                      self.speed[0],
+                      old_coords[1] + self.direction['V'] *
+                      self.speed[1])
         self.coords = new_coords
 
     def get_image(self):
-        """Returns the ball image
+        """Returns the pygame image object of the ball
         """
         return self.image
 
     def get_rect(self):
-        """Shows the ball's current position.
+        """Returns a pygame Rect object at the currect positon and size of the ball
         """
         ball_rect = pygame.Rect((self.coords), (self.diameter, self.diameter))
         return ball_rect
